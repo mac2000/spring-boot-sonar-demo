809a5696717ddf9c9e754f92a210fe52c33310c2
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-01-30 15:48:49 +0200

    sample test added

---
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+class CalculatorTest {
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void shouldAdd() {
+        assertThat(calculator.add(2, 2)).isEqualTo(4);
+    }
+}
